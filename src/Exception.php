a178bcfe1ec809219ae2f90b0119d163e3a72d76
KartaviK <varkuta.roma@gmail.com>
2018-11-30 17:57:53 +0200

    Implementation

---
+++ src/Exception.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wearesho\Delivery\Telegram;
+
+/**
+ * Class Exception
+ * @package Wearesho\Delivery\Telegram
+ */
+class Exception extends \Wearesho\Delivery\Exception
+{
+}
---
+++ src/Service.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wearesho\Delivery\Telegram;
+
+use Telegram\Bot\Api as TelegramApi;
+use Telegram\Bot\Exceptions\TelegramSDKException;
+use Wearesho\Delivery;
+
+/**
+ * Class Service
+ * @package Wearesho\Delivery\Telegram
+ */
+class Service implements Delivery\ServiceInterface
+{
+    /** @var TelegramApi */
+    protected $api;
+
+    public function __construct(TelegramApi $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * @param Delivery\MessageInterface $message
+     *
+     * @throws Exception
+     */
+    public function send(Delivery\MessageInterface $message): void
+    {
+        try {
+            $this->api->sendMessage([
+                'chat_id' => $message->getRecipient(),
+                'text' => $message->getText()
+            ]);
+        } catch (TelegramSDKException $exception) {
+            throw new Exception("Telegram bot error: " . $exception->getMessage(), $exception->getCode(), $exception);
+        }
+    }
+}
